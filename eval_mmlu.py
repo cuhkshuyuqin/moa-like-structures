22232959db8929c8071ace16c63a5aa053be5b66
cuhkshuyuqin <1155215619@link.cuhk.edu.hk>
2025-03-29 18:11:17 +0800

    now make directory before storing files

--- eval_mmlu.py
+++ eval_mmlu.py
@@ -2,15 +2,20 @@ from deepeval.benchmarks import MMLU
 import os
 
 from CustomTestModel import CustomTestModel
+from utils import LOG_DIR, RESULTS_DIR
+
 
 benchmark = MMLU()
 custom_test_model = CustomTestModel()
 
 benchmark.evaluate(model=custom_test_model)
 
-benchmark.predictions.to_csv(os.path.join("results", "mmlu", f"{START_TIME}_predictions.csv"))
-benchmark.task_scores.to_csv(os.path.join("results", "mmlu", f"{START_TIME}_task_scores.csv"))
-with open(os.path.join("results", "mmlu", f"{START_TIME}_overall_score.txt"), 'w') as file:
+current_results_dir = os.path.join(RESULTS_DIR, "arc_challenge")
+os.makedirs(current_results_dir, exist_ok=True)
+
+benchmark.predictions.to_csv(os.path.join(current_results_dir, f"{START_TIME}_predictions.csv"))
+benchmark.task_scores.to_csv(os.path.join(current_results_dir, f"{START_TIME}_task_scores.csv"))
+with open(os.path.join(current_results_dir, f"{START_TIME}_overall_score.txt"), 'w') as file:
     file.write(str(benchmark.overall_score))
-with open(os.path.join("logs", f"{START_TIME}_settings.txt"), 'w') as file:
+with open(os.path.join(LOG_DIR, f"{START_TIME}_settings.txt"), 'w') as file:
     file.write(SETTINGS_INFO)
--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ import os
 
 START_TIME = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-
 config = toml.load("config.toml")
 logging_config = config["logging"]
 inference_config = config["inference"]
@@ -19,6 +18,10 @@ VLLM_MODELS = inference_config["vllm"]
 VLLM_HOSTS = inference_config["vllm_hosts"]
 VLLM_PORTS = inference_config["vllm_ports"]
 
+LOG_DIR = "logs"
 
 logger.remove(0)
-logger.add(os.path.join("logs", f"{START_TIME}.log"))
+os.makedirs(LOG_DIR, exist_ok=True)
+logger.add(os.path.join(LOG_DIR, f"{START_TIME}.log"))
+
+RESULTS_DIR = "results"
