88dcaeb47b2415c74ad13058414e5eec41b2e4be
cuhkshuyuqin <1155215619@link.cuhk.edu.hk>
2025-03-29 18:00:42 +0800

    add 2 model structures to compare

--- model_structures.py
+++ model_structures.py
@@ -17,11 +17,49 @@ def structure_1(query):
 def structure_2(query):
     model = "Qwen/Qwen2-7B-Instruct"
 
-    proposer1 = Proposer(model, query)
-    proposer2 = Proposer(model, query)
+    layer0_1 = Proposer(model, query)
+    layer0_2 = Proposer(model, query)
+    layer0_3 = Proposer(model, query)
 
-    aggregator = Aggregator(
-        model, query, [proposer1, proposer2]
+    layer1_1 = Aggregator(
+        model, query, [layer0_1, layer0_2, layer0_3]
+    )
+    layer1_2 = Aggregator(
+        model, query, [layer0_1, layer0_2, layer0_3]
+    )
+    layer1_3 = Aggregator(
+        model, query, [layer0_1, layer0_2, layer0_3]
     )
 
-    return aggregator.generate()
+    layer2_1 = Aggregator(
+        model, query, [layer1_1, layer1_2, layer1_3]
+    )
+
+    return layer2_1.generate()
+
+
+def structure_3(query):
+    model = "Qwen/Qwen2-7B-Instruct"
+
+    layer0_1 = Proposer(model, query)
+    layer0_2 = Proposer(model, query)
+
+    layer1_1 = Aggregator(
+        model, query, [layer0_1, layer0_2]
+    )
+    layer1_2 = Aggregator(
+        model, query, [layer0_1, layer0_2]
+    )
+
+    layer2_1 = Aggregator(
+        model, query, [layer1_1, layer1_2]
+    )
+    layer2_2 = Aggregator(
+        model, query, [layer1_1, layer1_2]
+    )
+
+    layer3_1 = Aggregator(
+        model, query, [layer2_1, layer2_2]
+    )
+
+    return layer3_1.generate()
